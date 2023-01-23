2786db4ca24662262695ce7d04ca12791841bc61
FlandreScarlet <ciel_katana@outlook.com>
2023-01-23 13:45:41 +0800

    Update 树状数组.cpp

--- 数据结构/树状数组/树状数组.cpp
+++ 数据结构/树状数组/树状数组.cpp
@@ -40,3 +40,56 @@ struct BinaryIndexedTree {
 		return ret;
 	}
 };
+
+template <typename Type, typename Opt = std::plus<Type>>
+struct Fenwick_Tree {
+	Fenwick_Tree(int _n, const Type &val, const Opt &_opt) : bit(_n + 1, val), n(_n), opt(_opt) {}
+	void update(int x, const Type &d) {
+		for (int i = x; i <= n; i += i & -i)
+			bit[i] = opt(bit[i], d);
+	}
+	Type ask(int x, Type &&base_ret = {}) {
+		for (int i = x; i > 0; i -= i & -i) 
+			base_ret = opt(base_ret, bit[i]);
+		return base_ret;
+	}
+	auto &operator [] (int idx) {
+		return bit[idx];
+	}
+	const auto &operator [] (int idx) const {
+		return bit[idx];
+	}
+	auto &at(int idx) {
+		return bit[idx];
+	}
+	const auto &at(int idx) const {
+		return bit[idx];
+	}
+	auto &begin() {
+		return bit.begin();
+	}
+	const auto &cbegin() const {
+		return bit.cbegin();
+	}
+	auto &end() {
+		return bit.end();
+	}
+	const auto &cend() const {
+		return bit.cend();
+	}
+	auto &rbegin() {
+		return bit.rbegin();
+	}
+	const auto &crbegin() const {
+		return bit.crbegin();
+	}
+	auto &rend() {
+		return bit.rend();
+	}
+	const auto &crend() const {
+		return bit.crend();
+	}
+	int n;
+	const Opt &opt;
+	std::vector<Type> bit;
+};
