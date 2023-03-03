e3780d78d0d1761a510e65a1469a676714166a41
ChirunoC9 <ciel_katana@outlook.com>
2023-03-03 15:51:12 +0800

    模6判定更改

--- 数学/数论/素性判定/模6判定.cpp
+++ 数学/数论/素性判定/模6判定.cpp
@@ -12,7 +12,7 @@ bool __is_prime(int64_t n) {
 
 constexpr auto is_prime(int64_t n) noexcept -> bool {
 	if (n == 2 || n == 3 || n == 5) 
-		return false;
+		return true;
 	if (n <= 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
 		return false;
 	for (int64_t i = 5; i * i <= n; i += 6)
@@ -21,6 +21,23 @@ constexpr auto is_prime(int64_t n) noexcept -> bool {
 	return true;
 }
 
-int main() {
+auto Solve() -> void {
+	for (auto i : std::views::iota(1, 100)
+				| std::views::filter([](const auto &n) -> bool {
+						if (n == 2 || n == 3 || n == 5) 
+							return true;
+						if (n <= 1 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+							return false;
+						for (int64_t i = 5; i * i <= n; i += 6)
+							if (n % i == 0 || n % (i + 1) == 0)
+								return false;
+						return true;
+					})
+	) {
+		cout << i << ' ';
+	}
+}
 
+int main() {
+	
 }
