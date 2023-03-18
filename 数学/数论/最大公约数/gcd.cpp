e1016a3d89bf343c1ea0cc4414bd06b8d9d9cd4b
ChirunoC9 <ciel_katana@outlook.com>
2023-03-18 10:12:28 +0800

    Update gcd.cpp

--- 数学/数论/最大公约数/gcd.cpp
+++ 数学/数论/最大公约数/gcd.cpp
@@ -12,3 +12,26 @@ constexpr T gcd(T a, T b) {
 	};
 	return __gcd(__gcd, std::abs<T>(a), std::abs<T>(b));
 }
+
+template <typename T>
+	requires std::is_integral_v<T>
+constexpr auto __Gcd(T a, T b) -> T {
+	if (a == 0)
+		return a;
+	if (b == 0)
+		return b;
+	if (a % 2 == 0 && b % 2 == 0)
+		return 2 * __Gcd(a / 2, b / 2);
+	else if (a % 2 == 0)
+		return __Gcd(a / 2, n);
+	else if (b % 2 == 0)
+		return __Gcd(a, b / 2);
+	else
+		return __Gcd(std::abs(a - b), std::min(a, b));
+}
+
+template <typename T>
+	requires std::is_integral_v<T>
+constexpr auto Gcd(T a, T b) -> T {
+	return __Gcd(std::abs(a), std::abs(b));
+}
