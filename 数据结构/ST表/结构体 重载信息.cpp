d681bfbcd67c1d3307bba9f01800c24cd6a61063
ChirunoC9 <ciel_katana@outlook.com>
2023-03-19 10:24:19 +0800

    Update 结构体 重载信息.cpp

--- 数据结构/ST表/结构体 重载信息.cpp
+++ 数据结构/ST表/结构体 重载信息.cpp
@@ -1,21 +1,32 @@
 #include <bits/stdc++.h>
 
 namespace Data_Structure_SparseTable {
+	namespace __DS_ST_SP {
+		template <typename Ret>
+		struct Translate {
+			template <typename Val>
+				requires requires(Val v) { {Ret{v}}; }
+			constexpr auto operator () (const Val &v) const -> Ret {
+				return Ret{v};
+			}
+		};
+	}
+
 	template <typename Info>
 		requires requires(Info a, Info b) { {a + b} -> std::convertible_to<Info>; }
 	struct SparseTable {
 		int n;
 		std::vector<std::vector<Info>> f;
 
-		SparseTable(int _n, const Info &__init_v = Info{}) 
+		explicit SparseTable(int _n, const Info &__init_v = Info{}) 
 			: n(_n), f(std::__lg(_n) + 1, std::vector<Info>(_n + 1, __init_v)) {}
 
-		template <typename T, typename Container, typename Transfrom>
+		template <typename T, typename Container, typename Transfrom = __DS_ST_SP::Translate<Info>>
 			requires requires(Container a, int idx, T b, Transfrom func) {
 				{a[idx]} -> std::convertible_to<T>;
 				{func(b)} -> std::convertible_to<Info>;
 			}
-		auto init(const Container &a, const Transfrom &trans) -> void {
+		auto init(const Container &a, const Transfrom &trans = Transfrom{}) -> void {
 			for (int i = 1; i <= n; i ++) {
 				f[0][i] = trans(a[i]);
 			}
