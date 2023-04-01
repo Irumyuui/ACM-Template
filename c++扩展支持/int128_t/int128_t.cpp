045c8bb7d91f734cd80fa53694e454d16f54ac8d
ChirunoC9 <ciel_katana@outlook.com>
2023-04-01 23:04:07 +0800

    Update int128_t.cpp

--- c++扩展支持/int128_t/int128_t.cpp
+++ c++扩展支持/int128_t/int128_t.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 
-namespace support_for_int128 {
+namespace Royal_Guard {
+	std::istream& operator >> (std::istream &is, __int128_t &x) {
+		x = 0;
+		std::string s; is >> s;
+		bool flag = false;
+		for (auto ch : s)
+			if (ch == '-') flag = true;
+			else x = x * 10 + (ch ^ 48);
+		if (flag) x = -x;
+		return is;
+	}
+	std::ostream& operator << (std::ostream &os, __int128_t x) {
+		if (x == 0) return os << '0';
+		bool flag = false;
+		if (x < 0)
+			x = -x, flag = true;
+		std::string s;
+		while (x > 0)
+			s += x % 10 + '0', x /= 10;
+		if (flag) s += '-';
+		std::reverse(s.begin(), s.end());
+		return os << s;
+	}
 	using ill = __int128_t;
-	using i128 = __int128_t;
-	
-	
 }
