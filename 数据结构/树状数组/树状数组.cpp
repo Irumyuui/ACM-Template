9c719b6f1b771a739dd204146482d260c7df11c4
ChirunoC9 <ciel_katana@outlook.com>
2023-04-01 23:13:58 +0800

    Update 树状数组.cpp

--- 数据结构/树状数组/树状数组.cpp
+++ 数据结构/树状数组/树状数组.cpp
@@ -21,29 +21,27 @@ int aks(int l, int r) {
 	return ask(r) - ask(l - 1);
 }
 
-template <typename T>
-struct Fenwick_Tree {
-	int n;
-	std::vector<T> bit;
-	Fenwick_Tree(int _n, const T &v = {}) : bit(_n + 1, v), n(_n) {}
-	void add(int x, const T &d) {
-		for (int i = x; i <= n; i += i & -i) {
-			bit[i] += d;
+namespace Royal_Guard::Data_Structure::Basic_DS::Fenwick_Tree::Add {
+	template <typename T>
+	struct Fenwick_Tree {
+		int n;
+		std::vector<T> bit;
+		Fenwick_Tree(int _n, const T &v = {}) : bit(_n + 1, v), n(_n) {}
+		void add(int x, const T &d) {
+			for (int i = x; i <= n; i += i & -i) {
+				bit[i] += d;
+			}
 		}
-	}
-	auto ask(int x) -> T {
-		T ret = {};
-		for (int i = x; i > 0; i -= i & -i) {
-			ret += bit[i];
+		auto ask(int x) -> T {
+			T ret = {};
+			for (int i = x; i > 0; i -= i & -i) {
+				ret += bit[i];
+			}
+			return ret;
 		}
-		return ret;
-	}
-	auto ask(int l, int r) -> T {
-		return ask(r) - ask(l - 1);
-	}
-};
-
-int main() {
-	int n;
-
+		auto ask(int l, int r) -> T {
+			return ask(r) - ask(l - 1);
+		}
+	};
 }
+using Royal_Guard::Data_Structure::Basic_DS::Fenwick_Tree::Add::Fenwick_Tree;
