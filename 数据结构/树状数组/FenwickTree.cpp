71a06d5fdd8ab2b3c5ab47420743f953976b1d8f
ChirunoC9 <ciel_katana@outlook.com>
2023-06-27 11:21:34 +0800

    树状数组模板更新

---
+++ 数据结构/树状数组/FenwickTree.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+namespace RoyalGuard::DataStructure {
+	template <typename Type, typename Operator = std::plus<Type>>
+	#if __cplusplus >= 202002L
+	requires requires(Type a, Type b, Operator opt) {
+		{opt(a, b)} -> std::convertible_to<Type>;
+	}
+	#endif
+	class FenwickTree {
+		public:
+			FenwickTree(int size, Type initValue = Type{}, Operator opt = Operator{}) 
+				: bit(size + 1, initValue), opt(opt) {}
+
+			void Assign(int size, Type value) {
+				bit.Assign(size, value);
+			}
+
+			void Update(int idx, Type dx) {
+				for (int i = idx + 1; i < (int)bit.size(); i += Lowbit(i)) {
+					bit[i] = opt(bit[i], dx);
+				}
+			}
+
+			void Fill(Type value) {
+				std::fill(bit.begin(), bit.end(), value);
+			}
+
+			Type Get(int idx, Type result = {}) const {
+				for (int i = idx + 1; i > 0; i -= Lowbit(i)) {
+					result = opt(result, bit[i]);
+				}
+				return result;
+			}
+
+		private:
+			constexpr static int Lowbit(int x) noexcept {
+				return x & -x;
+			}
+
+			std::vector<Type> bit;
+			Operator opt;
+	};
+}
