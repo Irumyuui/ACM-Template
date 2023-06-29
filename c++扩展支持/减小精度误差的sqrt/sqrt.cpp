d52354bf734ceb07f2f498d1db84238651a75276
ChirunoC9 <ciel_katana@outlook.com>
2023-06-29 21:03:59 +0800

    Sqrt

--- c++扩展支持/减小精度误差的sqrt/sqrt.cpp
+++ c++扩展支持/减小精度误差的sqrt/sqrt.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 
-template <typename T>
-constexpr T Sqrt(const T &x) {
-	T ret = std::sqrt<T>(x);
-	while (ret * ret <= x) {
-		ret ++;
+namespace RoyalGuard::Math {
+	template <typename T>
+	constexpr T Sqrt(const T &x) {
+		T ret = std::sqrt<T>(x);
+		while (ret * ret <= x) {
+			ret ++;
+		}
+		while (ret * ret > x) {
+			ret --;
+		}
+		return ret;
 	}
-	while (ret * ret > x) {
-		ret --;
-	}
-	return ret;
 }
