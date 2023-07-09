da020c1908d816e78e08fe1eadd6c6ec639bd109
ChirunoC9 <ciel_katana@outlook.com>
2023-07-09 22:08:16 +0800

    Disjoint Set Union Template

---
+++ 数据结构/并查集/DisjointSetUnion.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+class DisjointSetUnion {
+	public:
+		explicit DisjointSetUnion(int size) 
+			: dad(size, -1) {}
+
+		int Find(int x) {
+			if (dad[x] <= -1) {
+				return x;
+			}
+			return dad[x] = Find(dad[x]);
+		}
+
+		int operator [] (int x) {
+			return Find(x);
+		}
+
+		bool Merge(int a, int b) {
+			a = Find(a); b = Find(b);
+			if (a == b)
+				return false;
+			if (a > b)
+				std::swap(a, b); 
+			dad[a] += dad[b];
+			dad[a] = b;
+			return true;
+		}
+
+		bool IsSame(int a, int b) {
+			return Find(a) == Find(b);
+		}
+
+		void Assign(int size, int value) {
+			dad.assign(size, value);
+		}
+
+		void Fill(int value = -1) {
+			std::fill(dad.begin(), dad.end(), value);
+		}
+
+		int GetSetSize(int x) {
+			return -dad[Find(x)];
+		}
+
+	private:
+		std::vector<int> dad;
+};
