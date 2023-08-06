c6dedf24bad2ee08184861a74ddf03886d290632
ChirunoC9 <ciel_katana@outlook.com>
2023-08-06 21:04:25 +0800

    树状数组

--- 数据结构/树状数组/FenwickTree.cpp
+++ 数据结构/树状数组/FenwickTree.cpp
@@ -3,52 +3,52 @@
 namespace RoyalGuard::DataStructure {
 
 #if __cplusplus >= 202002L
-	template <
-		typename Type,
-		std::regular_invocable<Type,Type> Operator = std::plus<Type>
-	>
+template <
+	typename Type,
+	std::regular_invocable<Type,Type> Operator = std::plus<Type>
+>
 #else
-	template <
-		typename Type,
-		typename Operator = std::plus<Type>,
-		std::enable_if<std::is_invocable<Operator,Type,Type>::value>* = nullptr
-	>
+template <
+	typename Type,
+	typename Operator = std::plus<Type>,
+	std::enable_if<std::is_invocable<Operator,Type,Type>::value>* = nullptr
+>
 #endif
-	class FenwickTree {
-	public:
-		explicit FenwickTree(int size, Type initValue = Type{}, Operator opt = Operator{}) 
-			: bit(size + 1, initValue), opt(opt) {}
+class FenwickTree {
+public:
+	explicit FenwickTree(int size, Type initValue = Type{}, Operator opt = Operator{}) 
+		: bit(size + 1, initValue), opt(opt) {}
 
-		auto Assign(int size, Type value) -> void {
-			assert(size >= 0);
-			bit.Assign(size, value);
+	auto Assign(int size, Type value) -> void {
+		assert(size >= 0);
+		bit.Assign(size, value);
+	}
+
+	auto Update(int idx, Type dx) -> void {
+		for (int i = idx + 1; i < (int)bit.size(); i += Lowbit(i)) {
+			bit[i] = opt(bit[i], dx);
 		}
+	}
 
-		auto Update(int idx, Type dx) -> void {
-			for (int i = idx + 1; i < (int)bit.size(); i += Lowbit(i)) {
-				bit[i] = opt(bit[i], dx);
-			}
+	auto Fill(Type value) -> void {
+		std::fill(bit.begin(), bit.end(), value);
+	}
+
+	auto Get(int idx, Type result = {}) const -> Type {
+		for (int i = idx + 1; i > 0; i -= Lowbit(i)) {
+			result = opt(result, bit[i]);
 		}
+		return result;
+	}
 
-		auto Fill(Type value) -> void {
-			std::fill(bit.begin(), bit.end(), value);
-		}
+private:
+	constexpr static
+	auto Lowbit(int x) noexcept -> int {
+		return x & -x;
+	}
 
-		auto Get(int idx, Type result = {}) const -> Type {
-			for (int i = idx + 1; i > 0; i -= Lowbit(i)) {
-				result = opt(result, bit[i]);
-			}
-			return result;
-		}
-
-	private:
-		constexpr static
-		auto Lowbit(int x) noexcept -> int {
-			return x & -x;
-		}
-
-		std::vector<Type> bit;
-		Operator opt;
-	};
+	std::vector<Type> bit;
+	Operator opt;
+};
 
 }
